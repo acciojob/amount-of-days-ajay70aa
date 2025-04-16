4e22aa09aab1f74b4e970441badffe1180cc1afe
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-16 20:47:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const outputElem = document.querySelector('.output')
+
+function daysOfAYear (year){
+	if(year%4===0 && year%100!==0 || year%400==0){
+		return 366
+	} else {
+		return 365
+	}
+}
+document.querySelector('.output').innerHTML = daysOfAYear(2024)
